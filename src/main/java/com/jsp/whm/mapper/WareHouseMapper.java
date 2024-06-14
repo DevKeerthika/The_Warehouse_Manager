d7a730542014c3fbb7722ca059eaca1325ac727c
DevKeerthika <keerthika.balu.cs@gmail.com>
2024-06-14 14:15:17 +0530

    FindWarehousebycity endpoint created

--- src/main/java/com/jsp/whm/mapper/WareHouseMapper.java
+++ src/main/java/com/jsp/whm/mapper/WareHouseMapper.java
@@ -1,23 +1,28 @@
 package com.jsp.whm.mapper;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import com.jsp.whm.entity.Address;
 import com.jsp.whm.entity.WareHouse;
 import com.jsp.whm.requestdto.WareHouseRequest;
+import com.jsp.whm.responsedto.AddressResponse;
 import com.jsp.whm.responsedto.WareHouseResponse;
 
 @Component
 public class WareHouseMapper 
 {
-	
+	@Autowired 
+	private AddressMapper  addressMapper;
+
 	public WareHouse mapToWareHouse(WareHouseRequest wareHouseRequest, WareHouse wareHouse)
 	{
 		wareHouse.setName(wareHouseRequest.getName());
 		wareHouse.setTotalCapacityInKg(0);
 		return wareHouse;
 	}
-	
+
 	public WareHouseResponse mapToWareHouseResponse(WareHouse wareHouse)
 	{
 		return WareHouseResponse.builder()
@@ -26,4 +31,13 @@ public class WareHouseMapper
 				.totalCapacityInKg(wareHouse.getTotalCapacityInKg())
 				.build();
 	}
+	
+	public WareHouseResponse mapToWareHouseResponse(WareHouse wareHouse, Address address)
+	{
+		WareHouseResponse response = mapToWareHouseResponse(wareHouse);
+		response.setAddressResponse(addressMapper.mapToAddressResponse(address));
+		return response;
+	}
+
+
 }
